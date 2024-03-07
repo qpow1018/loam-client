966295ab078b98c6c247a80f6aa22d6d569a63a7
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-03-07 16:06:27 +0900

    Feat: create Loatark define

---
+++ src/define/lostark.ts
@@ -0,0 +1,182 @@
+const LOSTARK = {
+  mainClassInfo: {
+    Warrior: { value: 'Warrior', label: '전사' },
+    MartialArtist: { value: 'MartialArtist', label: '무도가' },
+    Gunner: { value: 'Gunner', label: '헌터' },
+    Mage: { value: 'Mage', label: '마법사' },
+    Assassin: { value: 'Assassin', label: '암살자' },
+    Specialist: { value: 'Specialist', label: '스페셜리스트' },
+  },
+  classInfo: {
+    // start 전사
+    Berserker: {
+      value: 'Berserker',
+      label: '버서커',
+      mainClass: 'Warrior',
+      imageUrl: '',
+    },
+    Destroyer: {
+      value: 'Destroyer',
+      label: '디스트로이어',
+      mainClass: 'Warrior',
+      imageUrl: '',
+    },
+    Gunlancer: {
+      value: 'Gunlancer',
+      label: '워로드',
+      mainClass: 'Warrior',
+      imageUrl: '',
+    },
+    Paladin: {
+      value: 'Paladin',
+      label: '홀리나이트',
+      mainClass: 'Warrior',
+      imageUrl: '',
+    },
+    Slayer: {
+      value: 'Slayer',
+      label: '슬레이어',
+      mainClass: 'Warrior',
+      imageUrl: '',
+    },
+    // end 전사
+    // start 무도가
+    Wardancer: {
+      value: 'Wardancer',
+      label: '배틀마스터',
+      mainClass: 'MartialArtist',
+      imageUrl: '',
+    },
+    Scrapper: {
+      value: 'Scrapper',
+      label: '인파이터',
+      mainClass: 'MartialArtist',
+      imageUrl: '',
+    },
+    Soulfist: {
+      value: 'Soulfist',
+      label: '기공사',
+      mainClass: 'MartialArtist',
+      imageUrl: '',
+    },
+    Glaivier: {
+      value: 'Glaivier',
+      label: '창술사',
+      mainClass: 'MartialArtist',
+      imageUrl: '',
+    },
+    Striker: {
+      value: 'Striker',
+      label: '스트라이커',
+      mainClass: 'MartialArtist',
+      imageUrl: '',
+    },
+    Breaker: {
+      value: 'Breaker',
+      label: '브레이커',
+      mainClass: 'MartialArtist',
+      imageUrl: '',
+    },
+    // end 무도가
+    // start 헌터
+    Deadeye: {
+      value: 'Deadeye',
+      label: '데빌헌터',
+      mainClass: 'Gunner',
+      imageUrl: '',
+    },
+    Artillerist: {
+      value: 'Artillerist',
+      label: '블래스터',
+      mainClass: 'Gunner',
+      imageUrl: '',
+    },
+    Sharpshooter: {
+      value: 'Sharpshooter',
+      label: '호크아이',
+      mainClass: 'Gunner',
+      imageUrl: '',
+    },
+    Machinist: {
+      value: 'Machinist',
+      label: '스카우터',
+      mainClass: 'Gunner',
+      imageUrl: '',
+    },
+    Gunslinger: {
+      value: 'Gunslinger',
+      label: '건슬링어',
+      mainClass: 'Gunner',
+      imageUrl: '',
+    },
+    // end 헌터
+    // start 마법사
+    Arcanist: {
+      value: 'Arcanist',
+      label: '아르카나',
+      mainClass: 'Mage',
+      imageUrl: '',
+    },
+    Summoner: {
+      value: 'Summoner',
+      label: '서머너',
+      mainClass: 'Mage',
+      imageUrl: '',
+    },
+    Bard: {
+      value: 'Bard',
+      label: '바드',
+      mainClass: 'Mage',
+      imageUrl: '',
+    },
+    Sorceress: {
+      value: 'Sorceress',
+      label: '소서리스',
+      mainClass: 'Mage',
+      imageUrl: '',
+    },
+    // end 마법사
+    // start 암살자
+    Shadowhunter: {
+      value: 'Shadowhunter',
+      label: '데모닉',
+      mainClass: 'Assassin',
+      imageUrl: '',
+    },
+    Deathblade: {
+      value: 'Deathblade',
+      label: '블레이드',
+      mainClass: 'Assassin',
+      imageUrl: '',
+    },
+    Reaper: {
+      value: 'Reaper',
+      label: '리퍼',
+      mainClass: 'Assassin',
+      imageUrl: '',
+    },
+    Souleater: {
+      value: 'Souleater',
+      label: '소울이터',
+      mainClass: 'Assassin',
+      imageUrl: '',
+    },
+    // end 암살자
+    // start 스페셜리스트
+    Artist: {
+      value: 'Artist',
+      label: '도화가',
+      mainClass: 'Specialist',
+      imageUrl: '',
+    },
+    Aeromancer: {
+      value: 'Aeromancer',
+      label: '기상술사',
+      mainClass: 'Specialist',
+      imageUrl: '',
+    },
+    // end 스페셜리스트
+  }
+}
+
+export default LOSTARK;
