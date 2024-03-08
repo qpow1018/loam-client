5173346b09f2be584af93fbe3a598d31b352c23e
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-03-08 13:35:23 +0900

    Feat: update loaDB module

--- src/libs/loaDB.ts
+++ src/libs/loaDB.ts
@@ -1,11 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import LOSTARK from '@/define/lostark';
 import {
   LDB_MainClassInfo,
   LDB_ClassInfo,
   LDB_MainClassInfoAndClasses,
+  LDB_MyCharacterInfo,
 } from '@/types/loaDB';
 
+import { dataStorage, DataStorageKey } from '@/libs';
+
 class LoaDB {
+  // Game Data
   public getMainClassInfos(): LDB_MainClassInfo[] {
     return Object.values(LOSTARK.mainClassInfo);
   }
@@ -23,7 +29,26 @@ class LoaDB {
     }));
   }
 
+  // My Data
+  public getMyCharacters(): LDB_MyCharacterInfo[] {
+    return dataStorage.local.get(DataStorageKey.myCharacterList, []);
+  }
+
+  public addMyCharacter(nickname: string, classValue: string) {
+    const myCharacters = this.getMyCharacters();
+
+    const id = uuidv4();
+    const newData: LDB_MyCharacterInfo = { id, nickname, classValue };
+    const newList = [ ...myCharacters, newData ];
+
+    dataStorage.local.set(DataStorageKey.myCharacterList, newList);
+  }
+
 }
+
+
+
+
 
 const _instance = new LoaDB();
 export default _instance;
--- src/types/loaDB.ts
+++ src/types/loaDB.ts
@@ -15,8 +15,15 @@ type LDB_MainClassInfoAndClasses = {
   classes: LDB_ClassInfo[];
 }
 
+type LDB_MyCharacterInfo = {
+  id: string;
+  nickname: string;
+  classValue: string;
+}
+
 export type {
   LDB_MainClassInfo,
   LDB_ClassInfo,
   LDB_MainClassInfoAndClasses,
+  LDB_MyCharacterInfo,
 }
