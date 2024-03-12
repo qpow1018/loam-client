30f0991fefb4adc0aca2e3ff30ad315d79b7efde
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-03-12 15:44:03 +0900

    Feat: update LoadoMenuButton ui

--- src/app/loado/shared/LoadoMenuButton.tsx
+++ src/app/loado/shared/LoadoMenuButton.tsx
@@ -1,18 +1,45 @@
 import { ButtonBase } from '@mui/material';
+import theme from '@/style/theme';
+
+import Text from '@/components/Text';
 
 export default function LoadoMenuButton(
   props: {
+    onClick?: () => void;
     children: React.ReactNode;
-
-
+    iconComponent?: React.ReactNode;
   }
 ) {
   return (
     <ButtonBase
-    
-    
+      onClick={props.onClick}
+      sx={{
+        transition: theme.common.transition,
+        padding: '4px',
+        color: theme.color.text.secondary,
+        marginLeft: '6px',
+        '&:first-of-type': {
+          marginLeft: 0,
+        },
+        '& .MuiSvgIcon-root': {
+          fontSize: '18px',
+          marginRight: '4px'
+        },
+        '&:hover': {
+          color: theme.color.text.primary,
+        }
+      }}
     >
-      { props.children }
+      { props.iconComponent }
+
+      <Text
+        sx={{
+          fontSize: '0.75rem',
+          paddingBottom: '1px',
+        }}
+      >
+        { props.children }
+      </Text>
     </ButtonBase>
   );
 }
