c901e44bb1149028e0eded37737a640281d49ade
WonjinChoi <qpow1018@gmail.com>
2026-05-19 15:35:05 +0900

    feat: loado 기본 구조 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  sassOptions: {
+    additionalData: `
+      @use "@/assets/variables" as *;
+      @use "@/assets/mixins" as *;
+    `,
+  },
 };
 
 export default nextConfig;
--- src/app/loado/LoadoClient.tsx
+++ src/app/loado/LoadoClient.tsx
@@ -1,3 +1,12 @@
+import styles from "./loadoClient.module.scss";
+
 export default function LoadoClient() {
-  return <>test</>;
+  return (
+    <div className={styles["container"]}>
+      <h1 className={styles["title"]}>LoadoClient 테스트</h1>
+      <p className={styles["message"]}>
+        _variables.scss 의 변수가 자동 import 되는지 확인합니다.
+      </p>
+    </div>
+  );
 }
