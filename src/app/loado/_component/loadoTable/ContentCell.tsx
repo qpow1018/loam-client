113da8e539323ea1eb779830efa3ddca8d4c4173
WonjinChoi <qpow1018@gmail.com>
2026-05-26 18:55:44 +0900

    fix: 디자인 수정

--- src/app/loado/_component/loadoTable/ContentCell.tsx
+++ src/app/loado/_component/loadoTable/ContentCell.tsx
@@ -104,11 +104,11 @@ export default function ContentCell(props: {
 function renderStateIcon(state: TDisplayState) {
   switch (state) {
     case 'checked':
-      return <MdCheckBox size={20} />;
+      return <MdCheckBox size={18} />;
     case 'unchecked':
-      return <MdCheckBoxOutlineBlank size={20} />;
+      return <MdCheckBoxOutlineBlank size={18} />;
     case 'skip':
-      return <MdPause size={20} />;
+      return <MdPause size={18} />;
     case null:
       return null;
   }
