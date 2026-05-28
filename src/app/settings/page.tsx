b761d333f85ae288a1482c5474f02f44839f6d22
WonjinChoi <qpow1018@gmail.com>
2026-05-28 18:59:39 +0900

    Add Electron desktop shell

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,13 +1,5 @@
-export default function Dev() {
-  return (
-    <div>
-      <div>설정 페이지</div>
+import SettingsClient from './SettingsClient';
 
-      <div>
-        저장소 초기화, 수동 저장, 자동 저장 <br />
-        앱 다운로드
-        <br />
-      </div>
-    </div>
-  );
+export default function SettingsPage() {
+  return <SettingsClient />;
 }
