199072d79fe0ecb7c8dd1f01b291ee4680eecd94
WonjinChoi <qpow1018@gmail.com>
2026-06-01 16:20:57 +0900

    Rename Lostark response type

--- src/api/lostark/index.ts
+++ src/api/lostark/index.ts
@@ -1,9 +1,9 @@
 import apiRequest from '@/api/apiBase';
 
-import type { TLostarkSiblingCharactersResponse } from './type';
+import type { TResLostarkSiblingCharacters } from './type';
 
 export async function getSiblingCharacters(characterName: string) {
-  return apiRequest.post<TLostarkSiblingCharactersResponse>(
+  return apiRequest.post<TResLostarkSiblingCharacters>(
     '/lostark-sibling-characters',
     { characterName },
   );
--- src/api/lostark/type.ts
+++ src/api/lostark/type.ts
@@ -1,4 +1,4 @@
-export type TLostarkSiblingCharactersResponse = {
+export type TResLostarkSiblingCharacters = {
   ok?: boolean;
   status?: number;
   data: TLostarkSiblingCharacter[];
