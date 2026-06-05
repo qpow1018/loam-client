ba0dfb737bd0fb128f1f02ff86b0c681d595e2c3
WonjinChoi <qpow1018@gmail.com>
2026-06-05 16:03:18 +0900

    Update engraving section list UI

--- src/app/my-characters/_component/mainCharactersPanel/section/EngravingSection.tsx
+++ src/app/my-characters/_component/mainCharactersPanel/section/EngravingSection.tsx
@@ -5,44 +5,27 @@ import styles from './engravingSection.module.scss';
 export default function EngravingSection(props: { engravings: TLostarkEngraving[] }) {
   return (
     <section className={styles['engraving-section']}>
-      <div className={styles['section-header']}>
-        <h3 className={styles['title']}>각인</h3>
-        <span className={styles['count']}>{props.engravings.length}개</span>
-      </div>
+      <h3 className={styles['title']}>각인</h3>
 
-      {props.engravings.length > 0 ? (
-        <div className={styles['engraving-list']}>
-          {props.engravings.map((engraving, index) => (
-            <EngravingItem key={`${engraving.name ?? 'engraving'}-${index}`} engraving={engraving} />
-          ))}
-        </div>
-      ) : (
-        <p className={styles['empty']}>정보 없음</p>
-      )}
+      <div className={styles['engraving-list']}>
+        {props.engravings.map((engraving, index) => (
+          <div key={index} className={styles['engraving-item']}>
+            <div className={styles['name-box']}>
+              <span className={styles['marker']} />
+              <p className={styles['name']}>{engraving.name ?? '-'}</p>
+            </div>
+
+            <p
+              className={`${styles['level-value']} ${
+                engraving.level === 4 ? styles['max'] : ''
+              }`}
+            >
+              &times;
+              <span>{engraving.level ?? 0}</span>
+            </p>
+          </div>
+        ))}
+      </div>
     </section>
-  );
-}
-
-function EngravingItem(props: { engraving: TLostarkEngraving }) {
-  const { engraving } = props;
-
-  return (
-    <div className={styles['engraving-item']}>
-      <div className={styles['name-line']}>
-        <span className={styles['name']}>{engraving.name ?? '-'}</span>
-        {engraving.level !== null && <span className={styles['level']}>Lv. {engraving.level}</span>}
-      </div>
-
-      <div className={styles['meta-line']}>
-        {engraving.grade && <span className={styles['grade']}>{engraving.grade}</span>}
-        {engraving.abilityStoneLevel !== null && (
-          <span className={styles['stone-level']}>돌 +{engraving.abilityStoneLevel}</span>
-        )}
-      </div>
-
-      {engraving.description && (
-        <p className={styles['description']}>{engraving.description}</p>
-      )}
-    </div>
   );
 }
