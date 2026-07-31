5d26ab21e168fa82c98e0c19ddd996968d44a098
WonjinChoi <qpow1018@gmail.com>
2026-07-31 14:41:53 +0900

    refactor: align maplestory characters UI

--- src/app/maplestory/my-characters/page.tsx
+++ src/app/maplestory/my-characters/page.tsx
@@ -1,5 +1,11 @@
 import MyCharactersClient from './MyCharactersClient';
+import MaplestoryHeader from '@/components/maplestory/header/MaplestoryHeader';
 
 export default function MyCharactersPage() {
-  return <MyCharactersClient />;
+  return (
+    <>
+      <MaplestoryHeader />
+      <MyCharactersClient />
+    </>
+  );
 }
