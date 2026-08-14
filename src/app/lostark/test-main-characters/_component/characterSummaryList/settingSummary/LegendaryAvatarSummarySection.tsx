8a71e15cdbaa0cbb7e581738a5305ae9007490ab
WonjinChoi <qpow1018@gmail.com>
2026-08-14 16:20:05 +0900

    feat: refine legendary avatar summary

--- src/app/lostark/test-main-characters/_component/characterSummaryList/settingSummary/LegendaryAvatarSummarySection.tsx
+++ src/app/lostark/test-main-characters/_component/characterSummaryList/settingSummary/LegendaryAvatarSummarySection.tsx
@@ -9,26 +9,32 @@ const AVATAR_PARTS = ['머리', '상의', '하의', '무기'] as const;
 export default function LegendaryAvatarSummarySection(props: {
   avatars: TResLostarkCharacterSummary['legendaryAvatars'];
 }) {
-  const avatarCount = props.avatars.filter((avatar) =>
-    AVATAR_PARTS.some((part) => avatar.type?.includes(part)),
-  ).length;
+  const { avatars } = props;
+
+  function hasAvatar(part: (typeof AVATAR_PARTS)[number]) {
+    return avatars.some((avatar) => avatar.type?.includes(part));
+  }
 
   return (
     <SummarySection title="전설 아바타">
-      <div className={styles['avatar-list']}>
-        {AVATAR_PARTS.map((part) => (
-          <span
-            key={part}
-            className={
-              props.avatars.some((avatar) => avatar.type?.includes(part))
-                ? styles['avatar-equipped']
-                : styles['avatar-empty']
-            }
-          >
-            {part}
-          </span>
-        ))}
-        <b>{`${avatarCount} / 4`}</b>
+      <div className={styles['avatar-summary']}>
+        <div className={styles['avatar-list']}>
+          {AVATAR_PARTS.map((part) => (
+            <span
+              key={part}
+              className={`
+                ${styles['avatar-item']}
+                ${hasAvatar(part) ? styles['avatar-equipped'] : styles['avatar-empty']}
+              `}
+            >
+              {part}
+            </span>
+          ))}
+        </div>
+
+        <div className={styles['avatar-count-box']}>
+          갯수 <span className={styles['avatar-count']}>{`${avatars.length} / 4`}</span>
+        </div>
       </div>
     </SummarySection>
   );
